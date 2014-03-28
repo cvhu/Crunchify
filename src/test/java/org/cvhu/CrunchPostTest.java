2d6f0f111a087111614db670ca78acb2c353f3d3
cvhu <vic@cvhu.org>
2014-03-27 23:21:36 -0500

    added mockito and guice dependencies

--- src/test/java/org/cvhu/CrunchPostTest.java
+++ src/test/java/org/cvhu/CrunchPostTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class CrunchPostTest extends TestCase {
     
     public void testEscapedCsvString() {
-        CrunchPost post = new CrunchPost("\"company name\"", "http://companyname.com");
-        assertEquals("\"n/a\",", post.getCsvString());
+        CrunchPost post = new CrunchPost("company name", "http://companyname.com");
+        assertEquals("\"n/a\",\"n/a\",\"company name\",\"http://companyname.com\"\n", post.getCsvString());
     }
 }
